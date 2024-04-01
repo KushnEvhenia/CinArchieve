2adcac7146e6b4063c43797fecb2408285fd6684
KushnEvhenia <siel98765@gmail.com>
2024-04-01 13:34:53 +0300

    edited sql file

--- database.sql
+++ database.sql
@@ -1,15 +1,38 @@
--- Create the Movies database if it doesn't exist with UTF-8 encoding
-CREATE DATABASE IF NOT EXISTS Movies CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- --------------------------------------------------------
+-- Сервер:                       127.0.0.1
+-- Версія сервера:               10.1.48-MariaDB - mariadb.org binary distribution
+-- ОС сервера:                   Win64
+-- HeidiSQL Версія:              12.1.0.6537
+-- --------------------------------------------------------
 
--- Use the Movies database
-USE Movies;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Create the movies table with UTF-8 encoding
-CREATE TABLE IF NOT EXISTS movies (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  Title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-  Year INT,
-  Format VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-  Actors VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+
+-- Dumping database structure for Movies
+CREATE DATABASE IF NOT EXISTS `Movies` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `Movies`;
+
+-- Dumping structure for таблиця Movies.movies
+CREATE TABLE IF NOT EXISTS `movies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Year` int(11) DEFAULT NULL,
+  `Format` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Actors` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- Дані для експорту не вибрані
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
